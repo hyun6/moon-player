0d351a98d187fd7ff599feb837e7ceddc96c3c03
hyun6 <psymhs@gmail.com>
2022-10-09 16:54:52 +0900

    feat: player ui design with MUI
     - https://mui.com/material-ui/react-slider/#music-player

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import PlaybackPage from "./pages/playback.page";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PlaybackPage></PlaybackPage>
+    </div>
+  );
 }
 
 export default App;
